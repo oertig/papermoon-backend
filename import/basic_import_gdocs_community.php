0e8eaad161222cd4d33da8f81ba04c98d43575f8
oertig <147935788+oertig@users.noreply.github.com>
2024-10-06 23:30:31 +0200

    finish community import (v1)

--- import/basic_import_gdocs_community.php
+++ import/basic_import_gdocs_community.php
@@ -5,8 +5,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once __DIR__ . '/../config/config.inc.php';
+require_once __DIR__ . '/../modules/dbconn.module.php';
+
 $baseFilePathAndName = './raw_data/gdocs_csv_community/Upcoming Banners by Servant - %.csv';
-$typeNames = ['Saber', 'Archer', 'Lancer', 'Rider', 'Caster', 'Assassin', 'Berserker', 'Extra'];
+$typeNames = ['Saber', 'Archer', 'Lancer', 'Rider', 'Caster', 'Assassin', 'Berserker', 'EXTRA'];
+$allBanners = [];
 
 foreach($typeNames as $typeName) {
     $file = new SplFileObject(str_replace('%', $typeName, $baseFilePathAndName));
@@ -16,17 +20,17 @@ foreach($typeNames as $typeName) {
     $titleRow = true;
 
     foreach($file as $row) {
-        // go through rows
-        // print_r($row);
-
         if($titleRow) {
             $titleRow = false;
             continue;
         }
 
-        $bannerChunks = array_chunk(iterator_to_array($row), 3); // careful: there are non-visible elements! 3 is not correct!
-        $servantID = $bannerChunks[0][0];
-        unset($bannerChunks[0]);
+        $servantID = $row[0];
+        unset($row[0]); // number
+        unset($row[1]); // name
+        unset($row[2]); // empty cell
+
+        $bannerChunks = array_chunk(iterator_to_array($row), 5);
 
         foreach($bannerChunks as $bannerChunk) {
             if(empty($bannerChunk[0])) { // no more banners for current servant
@@ -34,11 +38,57 @@ foreach($typeNames as $typeName) {
             }
 
             $bannerName = $bannerChunk[0];
-            $startDate = $bannerChunk[1];
-            $endDate = $bannerChunk[2];
+            $bannerSolo = $bannerChunk[1];
+            $bannerSchedule = $bannerChunk[2]; // region? US vs JP?
+            $startDate = $bannerChunk[3];
+            $endDate = $bannerChunk[4];
+
+            if(!array_key_exists($bannerName, $allBanners)) {
+                $allBanners[$bannerName] = [
+                    'banner_solo' => $bannerSolo,
+                    'banner_schedule' => $bannerSchedule,
+                    'start_date' => $startDate,
+                    'end_date' => $endDate,
+                    'wiki_link' => '', // TODO: add wiki link
+                    'servants' => [],
+                ];
+            }
+
+            array_push($allBanners[$bannerName]['servants'], $servantID);
         }
     }
+}
 
-    die;
+uasort($allBanners, function($a, $b) {
+    $bannerDateFormat = 'Y/m/d';
+    $dateA = DateTimeImmutable::createFromFormat($bannerDateFormat, $a['start_date']);
+    $dateB = DateTimeImmutable::createFromFormat($bannerDateFormat, $b['start_date']);
+    return $dateA->getTimestamp() - $dateB->getTimestamp();
+});
+
+// echo '<pre>';
+// print_r($allBanners);
+
+foreach($allBanners as $bannerName => $banner) {
+    try {
+        $stmt = $pdo->prepare('INSERT INTO banner (`name`, `start_date`, end_date, wiki_link) VALUES (:name, :start_date, :end_date, :wiki_link)');
+        $stmt->bindParam(':name', $bannerName);
+        $stmt->bindParam(':start_date', $banner['start_date']);
+        $stmt->bindParam(':end_date', $banner['end_date']);
+        $stmt->bindParam(':wiki_link', $banner['wiki_link']);
+        $stmt->execute();
+
+        $bannerID = $pdo->lastInsertId();
+
+        foreach($banner['servants'] as $servantID) {
+            $stmt = $pdo->prepare('INSERT INTO banner_servant (`banner_fk`, `servant_fk`) VALUES (:banner_fk, :servant_fk)');
+            $stmt->bindParam(':banner_fk', $bannerID);
+            $stmt->bindParam(':servant_fk', $servantID);
+            $stmt->execute();
+        }
+    } catch (PDOException $e) {
+        echo "Error inserting banner: " . $e->getMessage() . "\n";
+        echo "<br><hr><br>";
+    }
 }
 ?>
