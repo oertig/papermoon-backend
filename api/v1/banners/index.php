71311f301633ab3e44bd8316502fc9f3995fe11f
oertig <147935788+oertig@users.noreply.github.com>
2024-10-23 22:49:45 +0200

    rework banner endpoint

--- api/v1/banners/index.php
+++ api/v1/banners/index.php
@@ -1,82 +1,177 @@
 <?php
-error_reporting(E_ALL);
 
-$requestMethod = $_SERVER['REQUEST_METHOD'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-// if($requestMethod !== 'POST') {
-//     http_response_code(405); // 405 = method not supported
-//     die();
-// }
+if($method !== 'GET') {
+    header('HTTP/1.1 405 Method Not Allowed');
+    header('Allow: GET');
+    exit;
+}
 
-// if(!in_array($_POST['request'], ['banner', 'servant'])) {
-//     http_response_code(400); // 400 = bad request
-//     die();
-// }
-
-require_once __DIR__ . '/../../../config/config.inc.php';
-require_once __DIR__ . '/../../../modules/dbconn.module.php';
+// ISO 8601 (YYYY-MM-DD) is widely used and recommended
+// eq equal, gt greater than, lt less than, gte greater than or equal, lte less than or equal
 
 /*
-example requests:
-
-/banner : get all banners
-/banners?id=* : get all banners
-/banners?id=1 : get banner 1
-/banners?status=active : get active banners // TODO: define possible values
-/banners?servant=1 : get banners for servant 1
-/banners?servant=1,2,3 : get banners for servants 1,2,3
-/banners?servant=all
+- banner
+    - id: comma-separated
+    - status: active, inactive, expired, upcoming
+    - 
+    - type: limited, story, permanent
+    - servant: by id, comma-separated
+- servant
+    - id
+    - rarity
+    - class
+------------------------------------------------------------
+/api/v1/banners?id=1,2,3
+/api/v1/banners?status=active
+/api/v1/banners?servant=1,2,3
+/api/v1/banners?id=1&servant=1,2,3&status=active
+GET /api/v1/banners?status=active&start_date[gte]=2024-08-01&end_date[lte]=2024-12-31
 */
 
-$bannerID = $_GET['id'] ?? null;
-$bannerStatus = $_GET['status'] ?? null;
-$bannerServants = $_GET['servant'] ?? null;
-$bannerServantIDs = [];
+$uri = $_SERVER['REQUEST_URI']; // to get the full path with query parameters after redirect from .htacess
+$components = parse_url($uri, PHP_URL_PATH);
+$path = $components['path']; // /api/v1/banners
+$requestedResource = ltrim($path, '/api/v1/');
+$query = $components['query']; 
+parse_str($query, $queryParams);
+// queryParams outputs: 
+// Array
+// (
+//     [status] => active
+//     [start_date] => Array
+//         (
+//             [gte] => 2024-08-01
+//         )
+// 
+//     [end_date] => Array
+//         (
+//             [lte] => 2024-12-31
+//         )
+// 
+// )
 
-if($bannerServants) { $bannerServantIDs = explode(',', $bannerServants); }
+switch($requestedResource) {
+    case 'banner':
+            // create sub-folder for each resource (to keep the code clean)
 
-$bannerQuery = "SELECT * FROM `banner` ORDER BY `start_date` ASC, `end_date` ASC"; // default to fetch all banners
+            $bannerQuery = "SELECT * FROM `banner` WHERE 1=1 ";
+            $queryParams = [];
+            $responseData = [];
 
-// TODO: implement better permutation-checking
-// if($bannerID === '*') {
-//     if($bannerStatus === 'active') {
-//         if(!empty($bannerServantIDs)) {
-//             // get active banners for specified servants
-//         } else {
-//             // get all active banners
-//         }
-//     } else {
-//         // get all banners
-//     }
-// } else {
-//     // get specific banners
-// }
+            if(isset($queryParams['id']) && isValidIntegerArrayQueryParameter($queryParams['id'])) {
+                $bannerQuery .= "AND `id` IN (:bannerIds) ";
+                $queryParams['bannerIds'] = $queryParams['id'];
+            }
 
-$status = 'success';
+            if(isset($queryParams['servant']) && isValidIntegerArrayQueryParameter($queryParams['servant'])) {
+                $bannerQuery .= "AND `servant_fk` IN (:servantIds) ";
+                $queryParams['servantIds'] = $queryParams['servant'];
+            }
 
-try {
-    $stmt = $pdo->prepare($bannerQuery);
-    $stmt->execute();
-    $banners = $stmt->fetchAll(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    $status = 'error';
-    $errorCode = $pdo->errorCode();
-    $errorMessage = $pdo->errorInfo();
+            // if status equals 'all' or '*', do nothing
+            // status is mutually exclusive with each other
+            if (isset($queryParams['status']) && in_array($queryParams['status'], ['active', 'inactive', 'expired', 'upcoming'])) {
+                $currentDate = date('Y-m-d');
+
+                switch($queryParams['status']) {
+                    case 'active':
+                        $bannerQuery .= "AND `start_date` <= '$currentDate' AND `end_date` >= '$currentDate' ";
+                        break;
+                    case 'inactive':
+                        $bannerQuery .= "AND `start_date` > '$currentDate' OR `end_date` < '$currentDate' ";
+                        break;
+                    case 'expired':
+                        $bannerQuery .= "AND `end_date` < '$currentDate' ";
+                        break;
+                    case 'upcoming':
+                        $bannerQuery .= "AND `start_date` > '$currentDate' ";
+                        break;
+                    default:
+                            // not supported status
+                            // TODO: return error message
+                        break;
+                }
+            }
+
+            /**********************************************************************************************************/
+
+            $dateOperators = [
+                'gte' => '>=',
+                'gt' => '>',
+                'lte' => '<=',
+                'lt' => '<',
+                'eq' => '=',
+            ];
+
+            $dateFormat = 'Y-m-d';
+
+            foreach(array('start_date', 'end_date') as $dateType) {
+                if (isset($queryParams[$dateType]) && !is_array($queryParams[$dateType])) {
+                    $queryParams[$dateType] = ['eq' => $queryParams[$dateType]];
+                }
+    
+                if(isset($queryParams[$dateType])) {
+                    $dateString = $queryParams[$dateType][0];
+                    $date = DateTime::createFromFormat($dateFormat, $dateString);
+
+                    if(!$date || $date->format($dateFormat) !== $dateString) {
+                        // date is invalid or not in the correct format
+                        throw new Exception("Invalid date format for $dateType");
+                    }
+
+                    $bannerQuery .= "AND `".$dateType."` ".$dateOperators[$queryParams[$dateType][0]]." :".$dateType." ";
+                    $queryParams[$dateType] = $queryParams[$dateType][0];
+                }
+            }
+
+            /**********************************************************************************************************/
+
+            $stmt = $pdo->prepare($bannerQuery); // after the query has been concatenated
+
+            foreach ($queryParams as $key => $value) {
+                $stmt->bindValue(":" . $key, $value);
+            }
+
+            try {
+                $stmt->execute();
+                $banners = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $responseData['banners'] = $banners;
+            } catch(PDOException $e) {
+                // TODO: handle error better (no direct output to the browser)
+                // TODO: add logging
+
+                http_response_code(500);
+                $responseData['error'] = [
+                    'code' => $e->getCode(),
+                    'error' => $e->getMessage(),
+                ];
+            }
+
+            header('Content-Type: application/json');
+            echo json_encode($responseData); // TODO: add more data
+
+        break;
+    case 'servant':
+            // TODO: implement logic for servants
+        break;
+    default:
+            // not supported resource
+            // TODO: return error message
+        break;
 }
 
-$apiResponse = [
-    'api_version' => $apiV1Version,
-    'status' => $status,
-    'amount_banners' => count($banners),
-    'data' => $banners,
-];
+function isValidIntegerArrayQueryParameter($parameter) {
+    $elements = explode(',', $parameter);
 
-if($status !== 'success') {
-    $apiResponse['error'] = [ // TODO: implement risk-free error message (don't show pdo message and code to users)
-        'code' => $errorCode,
-        'message' => $errorMessage,
-    ];
+    foreach ($elements as $value) {
+        if (!ctype_digit($value)) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-echo json_encode($apiResponse, JSON_UNESCAPED_SLASHES);
 ?>
